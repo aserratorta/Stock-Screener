52f86819892741dd14e2b971e313e48099c58fce
Anton Serra <aserratorta@gmail.com>
2016-02-13 19:43:13 +0100

    create traits, entity and repo

---
+++ src/AppBundle/Entity/SuperSector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 13/2/16
+ * Time: 16:13
+ */
+
+namespace AppBundle\Entity;
+
+
+class SuperSector
+{
+
+}
---
+++ src/AppBundle/Entity/Traits/TitleTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 13/2/16
+ * Time: 19:19
+ */
+
+namespace AppBundle\Entity\Traits;
+
+
+class TitleTrait
+{
+
+}
