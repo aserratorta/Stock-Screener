256376bf0a55eb4d0bd4ec25430c8a32d026ebad
Anton Serra <anton@Mac-mini-de-Anton.local>
2016-05-09 18:14:07 +0200

    add and configure FilterController

---
+++ src/AppBundle/Admin/Block/MyBlock.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Admin\Block;
+
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class MyBlock
+ *
+ * @category Block
+ * @package  AppBundle\Admin\Block
+ * @author   David Romaní <david@flux.cat>
+ */
+class MyBlock extends BaseBlockService
+{
+    /**
+     * Execute
+     *
+     * @param BlockContextInterface $blockContext
+     * @param Response              $response
+     *
+     * @return Response
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        return $this->renderResponse(
+            $blockContext->getTemplate(),
+            array(
+                'block'           => $blockContext->getBlock(),
+                'settings'        => $blockContext->getSettings(),
+                'title'           => 'myblock',
+            ),
+            $response
+        );
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'myblock';
+    }
+
+    public function configureSettings(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'title'    => 'Resume',
+                'content'  => 'Default content',
+                'template' => '::Admin/Blocks/block_screener.html.twig',
+            )
+        );
+    }
+}
---
+++ src/AppBundle/Controller/Admin/ScreenerAdminController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class ScreenerAdminController
+ *
+ * @category Controller
+ * @package  AppBundle\Controller\Admin
+ * @author   David Romaní <david@flux.cat>
+ */
+class ScreenerAdminController extends BaseAdminController
+{
+    /**
+     * Filter action
+     *
+     * @param int|string|null $id
+     * @param Request         $request
+     *
+     * @return Response
+     * @throws NotFoundHttpException If the object does not exist
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function filterAction(Request $request = null)
+    {
+        $request = $this->resolveRequest($request);
+
+        //TODO
+
+//        $form = $this->createForm(ContactMessageAnswerType::class, $object);
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+            // persist new contact message form record
+//            $object->setAnswered(true);
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($object);
+//            $em->flush();
+//        }
+
+        return $this->render(
+            '::Admin/Filters/filters_form.html.twig',
+            array(
+                'action'   => 'show',
+                'object'   => array(),
+                'elements' => $this->admin->getShow(),
+            ),
+            null,
+            $request
+        );
+    }
+}
